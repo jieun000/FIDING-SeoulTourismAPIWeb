17897271e15ae120c9dc230e6bf742cf6bb2b7a8
jieun <mondieu0@naver.com>
2023-11-22 11:43:36 +0900

    회원가입 start와 네이버 & 카카오 연동 합침

---
+++ jieun/02_2st_main/src/main/java/com/seoul/fiding/vo/MemberVO.java
@@ -0,0 +1,12 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private  int mno;
+	private String name;
+	private String password;
+	private String address;
+	private String email;
+}
