852670c15af61c688dc263fdae5a6defa4cb39f1
jiyeonggg <wldud6567@nate.com>
2023-11-23 13:25:22 +0900

    css 최종

---
+++ jiyoung/2st_main_1123/src/main/java/com/seoul/fiding/vo/MemberVO.java
@@ -0,0 +1,14 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String id;
+	private String pw;
+	private String username;
+	private String nickname;
+	private String phone;
+	private String email;
+	
+}
