e4aa981356870044bc9a3c8923569978c4ef2834
jiyeonggg <wldud6567@nate.com>
2023-11-27 18:03:19 +0900

    지영꺼

---
+++ jiyoung/2st_main_jy/src/main/java/com/seoul/fiding/vo/TourVO.java
@@ -0,0 +1,13 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class TourVO {
+
+	private int TL_NO ;
+	private String cat;
+	private String address;
+	private int search_cnt ;
+	
+}
