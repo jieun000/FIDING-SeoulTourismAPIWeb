d7468daa26946b207d7a8e85b1462385f5264bd7
제이 <jeyru1114@gmail.com>
2023-11-29 23:35:57 +0900

    게시물,수정,삭제 최종수정

---
+++ eunyoung/1120_sts/2st_main_1129_최종수정/src/main/java/com/seoul/fiding/vo/SearchVO.java
@@ -0,0 +1,13 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class SearchVO {
+
+	private int page_num ;
+	private String search;
+	private String id;
+	private String cat;
+	
+}
