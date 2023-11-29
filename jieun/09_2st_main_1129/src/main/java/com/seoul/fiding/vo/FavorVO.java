2f0aeada840f52d040b43af3f1da82764fcb6877
jieun <mondieu0@naver.com>
2023-11-29 18:02:22 +0900

    1129

---
+++ jieun/09_2st_main_1129/src/main/java/com/seoul/fiding/vo/FavorVO.java
@@ -0,0 +1,13 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class FavorVO {
+
+	private int FL_NO ;
+	private int BD_NO ;
+	private String WRITER_ID;
+	private String USER_ID;
+	
+}
