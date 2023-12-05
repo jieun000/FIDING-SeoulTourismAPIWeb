d480537210b97bd9288cf5e41e90c260fa4c98e4
제이 <jeyru1114@gmail.com>
2023-12-05 17:19:15 +0900

    src는 vscode 리액트에 lastpang은 sts프로젝트

---
+++ team/2차/2차/src/main/java/com/seoul/fiding/vo/LikeVO.java
@@ -0,0 +1,13 @@
+package com.seoul.fiding.vo;
+
+import lombok.Data;
+
+@Data
+public class LikeVO {
+
+	private int LL_NO ;
+	private int BD_NO ;
+	private String WRITER_ID;
+	private String USER_ID;
+	
+}
