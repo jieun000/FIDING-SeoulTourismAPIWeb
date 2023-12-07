5ca6e3b0412bdb9b33ba5d0866f1a243d4ad00ff
jiyeonggg <wldud6567@nate.com>
2023-12-07 15:04:58 +0900

    합체 준비

--- jiyoung/project3/frontend/src/components/Navigration.js
+++ jiyoung/project3/frontend/src/components/Navigration.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import "./navigation.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavItem from "./NavItem";
 import { useNavigate } from 'react-router-dom';
 
 function Navigation() {
   const [menuToggle, setMenuToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   const menu = [
     { address: "/", src:"logo.png" },
     { name: "로그인", address: "/login" },
-    { name: "회원가입", address: "/signup" },
+    { name: "회원가입", address: "/signup" }, 
+    <br/>
   ];
 
-    const navigate = useNavigate();
-    
-    const redirectToMain = () => {
-      // React Router의 navigate 함수를 사용하여 이동
-      navigate('/');
-    }
+  const navigate = useNavigate();
+  
+  const redirectToMain = () => {
+    // React Router의 navigate 함수를 사용하여 이동
+    navigate('/');
+  }
 
-  return (
-    <nav className="navigation__wrapper">
+  useEffect(() => {
+    const handleScroll = () => {
+      const menuVisible = document.querySelector('.menu__box__visible');
+      if (menuVisible) {
+        if (window.scrollY > 76) {
+          setScrolled(true);
+          menuVisible.style.top = '0';
+        } else {
+          setScrolled(false);
+          menuVisible.style.top = '76px';
+        }
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
+  
+    return ( 
+      <nav className={`navigation__wrapper ${scrolled ? 'scrolled' : ''}`}>
       <div
         className={!menuToggle ? "burger__menu" : "x__menu"}
         onClick={() =>
